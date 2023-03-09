aac24fe1994b47b8aa2e90c8c783a21e9ea11b2e
Graham Downs <graham@communicate.za.net>
2023-03-09 12:30:56 +0200

    Add Settings class with ExchangeRateApiKey

---
+++ ZarCurrencyTooter/Settings.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace ZarCurrencyTooter
+{
+    internal class Settings
+    {
+        private const string SettingsFileName = "settings.json";
+
+        public string ExchangeRateApiKey { get; set; }
+
+        public static Settings Load()
+        {
+            if (!File.Exists(SettingsFileName))
+                return new Settings();
+
+            var text = File.ReadAllText(SettingsFileName);
+            return JsonConvert.DeserializeObject<Settings>(text) ??
+                   throw new ApplicationException($"Your '{SettingsFileName}' appears to be empty or corrupt.");
+        }
+
+        public void Save()
+        {
+            var serialised = JsonConvert.SerializeObject(this);
+            File.WriteAllText(SettingsFileName, serialised);
+        }
+    }
+}
