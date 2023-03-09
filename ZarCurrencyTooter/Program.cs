cffd94ac2f91b26682bd28dbf2f472d67ac399fc
Graham Downs <graham@communicate.za.net>
2023-03-09 11:59:44 +0200

    Add solution and project

---
+++ ZarCurrencyTooter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ZarCurrencyTooter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
