abeed08eb4cc1664204a2d6c2e4a7a65e82ca5ba
Graham Downs <graham@communicate.za.net>
2023-03-29 13:50:04 +0200

    Add validation for Exchange Rate API key

--- ZarCurrencyTooter/CurrenciesApiClient.cs
+++ ZarCurrencyTooter/CurrenciesApiClient.cs
@@ -9,13 +9,26 @@ namespace ZarCurrencyTooter
         public async Task<CurrencyLatestResults> GetLatest(string apiKey, string baseCurrencyCode,
             List<string> compareCurrencyCodes)
         {
+            if (string.IsNullOrEmpty(apiKey))
+                throw new ApplicationException("Exchange Rate API Key not specified");
+
             var restClient = new RestClient("https://api.apilayer.com/");
             var symbols = ConvertCurrencyCodesToCsv(compareCurrencyCodes);
             var request = new RestRequest($"exchangerates_data/latest?base={baseCurrencyCode}&symbols={symbols}");
             request.AddHeader("apikey", apiKey);
-            var response = await restClient.GetAsync(request);
-            var results = JsonConvert.DeserializeObject<CurrencyLatestResults>(response.Content);
-            return results;
+            try
+            {
+                var response = await restClient.GetAsync(request);
+                var results = JsonConvert.DeserializeObject<CurrencyLatestResults>(response.Content);
+                return results;
+            }
+            catch (HttpRequestException e)
+            {
+                if (e.Message.Contains("Unauthorized"))
+                    throw new ApplicationException("Invalid Exchange Rate API Key");
+
+                throw;
+            }
         }
 
         private string ConvertCurrencyCodesToCsv(List<string> currencyCodes)
